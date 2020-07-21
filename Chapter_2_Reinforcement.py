6ac5df5d358e151095bd3e3824783995c02bc376
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-21 12:03:13 -0700

    Update Chapter_2_Reinforcement.py

--- Chapter_2_Reinforcement.py
+++ Chapter_2_Reinforcement.py
@@ -59,3 +59,62 @@ R-2.5 Use the techniques of Section 1.7 to revise the charge and make_payment
 methods of the CreditCard class to ensure that the caller sends a number
 as a parameter
 """
+
+class CreditCard:
+    """A consumer credit card."""
+    
+    def __init__(self, customer, bank, acnt, limit):
+        """Create a new credit card instance.
+        the initial balance is zero
+        customer the name of the customer (e.g., John Bowman )
+        bank the name of the bank (e.g., California Savings )
+        acnt the acount identifier (e.g., 5391 0375 9387 5309 )
+        limit credit limit (measured in dollars)"""
+        
+        
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+        
+    def get_customer(self):
+        """Return name of the customer"""
+        return self._customer
+    
+    def get_bank(self):
+        """return the banks name"""
+        return self._bank
+    
+    def get_account(self):
+        """Return the card identifying number (typically stored as a string)."""
+        return self._account
+    
+    def get_balance(self):
+        """Return current balance."""
+        return self._balance
+    
+    def charge(self,price):
+        """ Charge given price to the card, assuming sufficient credit limit.
+        return True if chage was processed; False if charge was denied."""
+        
+        try:
+            if price + self._balance > self._limit: #If charge would exceed limit,
+                return "Transaction Error, inceficient funds."
+            else:
+                self._balance += price
+                return "Thank you for your transaction!"
+        except:
+            return "Looks like there was an error with this charge. Please attempt to charge the card only in USD"
+        
+        #lse:
+            #eturn 
+    def make_payment(self, amount):
+        """Checks if input is valid and process customer payment that reduces balance"""
+        try:
+            self._balance -= amount
+        except:
+            return "Looks like that you haven't input a numeric amount"
+        
+        else:
+            return "Thank you for your payment!"
