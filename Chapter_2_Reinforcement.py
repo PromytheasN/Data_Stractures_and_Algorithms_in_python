1c278383cf08e6551078a49ee0ab5cef0f94bb74
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-20 17:28:15 -0700

    Update Chapter_2_Reinforcement.py

--- Chapter_2_Reinforcement.py
+++ Chapter_2_Reinforcement.py
@@ -1,1 +1,27 @@
+"""
+R-2.1 Give three examples of life-critical software applications.
+"""
+#1) Critical life support machine software
+#2) Auto-drive (for self drive vehicles) software
+#3) Nuclear reactors software
 
+"""
+R-2.2 Give an example of a software application in which adaptability can mean
+the difference between a prolonged lifetime of sales and bankruptcy.
+"""
+#Any trademarked Operating System (eg. Windows)
+
+"""
+R-2.3 Describe a component from a text-editor GUI and the methods that it encapsulates.
+"""
+
+
+
+
+"""
+R-2.4 Write a Python class, Flower, that has three instance variables of type str,
+int, and float, that respectively represent the name of the flower, its number of petals, and its price. Your class must include a constructor method
+that initializes each variable to an appropriate value, and your class should
+include methods for setting the value of each type, and retrieving the value
+of each type
+"""
