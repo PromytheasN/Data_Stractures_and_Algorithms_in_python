235cc979582b8ac64145b77f5835c433653bfabc
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-28 12:37:40 -0700

    Update Chapter_2_Reinforcement.py

--- Chapter_2_Reinforcement.py
+++ Chapter_2_Reinforcement.py
@@ -351,3 +351,66 @@ the difference we divide it by the stp in order to find the total amount of step
 
 
 
+"""R-2.18 Give a short fragment of Python code that uses the progression classes
+from Section 2.4.2 to find the 8th value of a Fibonacci progression that
+starts with 2 and 2 as its first two values."""
+
+class Progression:
+    """Iterator produicing a generic progression.
+    Default iterator producces the whole numbers 0,1,2,..."""
+    
+    def __init__(self,start=0):
+        
+        """Initialize current to the first value of the progression."""
+        self._current = start
+        
+    def _advance(self):
+        """Update self._current to a new value.
+
+        This shouldd be overriden by a subclass to customize progression.
+        By convention, if current is set to None, this desigantes the end of 
+        a finite progression"""
+
+        self._current += 1
+        
+    def __next__(self):
+        
+        """Return the next element, or else raise StopIteration error."""
+        
+        if self._current is None: #our convention to end a progression
+            raise StopIteration()
+        else:
+            answer = self._current #record current value to return
+            self._advance() #advance to prepare for next time
+            return answer #return the answer
+        
+    def __iter__(self):
+        """By convention, an iterator must return itself as an iterator"""
+        return self
+    
+    def print_progression(self,n):
+        """Print next n values of the progression."""
+        print(" ".join(str(next(self))for j in range(n)))
+        
+class FibonacciProgression(Progression):
+    """Iterator producing a generalized Fibonacci progression."""
+    
+    def __init__(self, first = 0, second = 1):
+        """Create a new fibonacci progression.
+        
+        first the first term of the progression(default 0)
+        second the second term of progression(default 1)
+        """
+        
+        super().__init__(first) #starts progression at first
+        self._prev = second - first #fictious value preceding the first
+        
+    def _advance(self):
+        """Update current value by taking sum of previous two."""
+        
+        self._prev, self._current = self._current, self._prev + self._current
+        
+#This code will produce the fibonacci progression up to 8th value starting with 2 and 2
+FibonacciProgression(2,2).print_progression(8)
+
+        
